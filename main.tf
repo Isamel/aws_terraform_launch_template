bac8e9aaf14a6e85c4b31b5cf52851c17ab5e582
unknown <Jose Salinas>
2020-06-13 21:52:05 -0700

    main

---
+++ main.tf
@@ -0,0 +1,14 @@
+resource "null_resource" "depends_on" {
+    triggers = {
+        depends_on = join("", var.lb_target_group_attachment_depends_on)
+    }
+}
+
+resource "aws_launch_template" "launch_template_terraform" {
+    count         = var.launch_template_count
+    depends_on    = [null_resource.depends_on]
+    name_prefix   = var.launch_template_name_prefix
+    image_id      = var.launch_template_image_id
+    instance_type = var.launch_template_instance_type
+    tags          = local.tags
+}
